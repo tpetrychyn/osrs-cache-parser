75cfb867a75d8792c6f9e8d940a487035789f510
tpetrychyn <tpetrychyn@gmail.com>
2020-02-28 17:32:43 -0600

    working on gzip, not working

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"compress/bzip2"
+	"compress/gzip"
 	"encoding/binary"
 	"fmt"
 	"log"
@@ -10,6 +11,8 @@ import (
 )
 
 const INDEX_ENTRY_LENGTH = 6
+
+var BZIP_HEADER = []byte{66,90,104,49}
 
 func main() {
 	f, err := os.OpenFile("./cache/main_file_cache.idx255", os.O_RDONLY, 0644)
@@ -42,15 +45,15 @@ func main() {
 		compression, _ := reader.ReadByte()
 		var compressedLength int32
 		_ = binary.Read(reader, binary.BigEndian, &compressedLength)
-		log.Printf("compressed length: %d", compressedLength)
-		data := make([]byte, compressedLength)
+
+		var data []byte
 		switch compression {
 		case 0: // no compression
+			data = make([]byte, compressedLength)
 			encryptedData := make([]byte, compressedLength)
 			reader.Read(encryptedData)
 
 			data = encryptedData
-
 		case 1: // BZ2
 			encryptedData := make([]byte, compressedLength + 4)
 			reader.Read(encryptedData)
@@ -58,15 +61,42 @@ func main() {
 			stream := bytes.NewReader(encryptedData)
 			var decompressedLength int32
 			binary.Read(stream, binary.BigEndian, &decompressedLength)
+
 			comp := make([]byte, compressedLength)
 			stream.ReadAt(comp, 4)
-			comp = append([]byte{66,90,104,49}, comp...)
+			comp = append(BZIP_HEADER, comp...)
 			bz := bzip2.NewReader(bytes.NewReader(comp))
+
+			data = make([]byte, decompressedLength)
 			_, err := bz.Read(data)
 			if err != nil {
 				panic(fmt.Sprintf("unable to bzip decompress index %d: %s", indexEntry.Id, err.Error()))
 			}
 		case 2: // GZ
+			encryptedData := make([]byte, compressedLength + 4)
+			reader.Read(encryptedData)
+
+			stream := bytes.NewReader(encryptedData)
+
+			var decompressedLength int32
+			binary.Read(stream, binary.BigEndian, &decompressedLength)
+
+			comp := make([]byte, compressedLength)
+			stream.ReadAt(comp, 4)
+
+			gz, err := gzip.NewReader(bytes.NewReader(comp))
+			if err != nil {
+				panic(err)
+			}
+
+			data = make([]byte, decompressedLength)
+			n, err := gz.Read(data)
+			if n != int(decompressedLength) {
+					panic(fmt.Errorf("bytes read from gzip %d did not match decompressedLength %d", n, decompressedLength))
+			}
+			if err != nil {
+				// TODO: ignoring for now since it seems to work and just be EOF
+			}
 		default:
 			panic("unknown compression type")
 		}
